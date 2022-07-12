80b77cd39272475a3cc3cfc7f358f7db906a9b33
alxhzhong <108833315+alxhzhong@users.noreply.github.com>
2022-07-12 14:30:58 -0400

    changing intervals

--- SIR_intervals.R
+++ SIR_intervals.R
@@ -6,9 +6,9 @@ starting_param_val = log(c(1e-2,1e-5))
 date_initial = as.Date("2020-11-22")
 date_final = as.Date("2021-03-01")
 
-t1 <- sir_all(mexico, "2020-11-22", "2020-12-15", starting_param_val)
-t2 <- sir_all(mexico, "2020-12-26", "2021-01-08", t1[[3]]) 
-t3 <- sir_all(mexico, "2021-01-09", "2021-01-22", t2[[3]])
+t1 <- sir_all(mexico, "2020-11-22", "2020-12-22", starting_param_val)
+t2 <- sir_all(mexico, "2020-12-23", "2021-01-11", t1[[3]]) 
+t3 <- sir_all(mexico, "2021-01-12", "2021-01-22", t2[[3]])
 t4 <- sir_all(mexico, "2021-01-23", "2021-03-01", t3[[3]])
 
 pred_I <- rbind(t1[[1]], t2[[1]], t3[[1]], t4[[1]]) ## bind by row
