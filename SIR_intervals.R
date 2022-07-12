1923c85d9aa2b55478519ed1d3ddbf7bbf7b7a9b
alxhzhong <108833315+alxhzhong@users.noreply.github.com>
2022-07-12 15:13:31 -0400

    fixed intervals, allowing for change between SIR and SEIR

--- SIR_intervals.R
+++ SIR_intervals.R
@@ -1,17 +1,33 @@
 
 source("SIR_function.R")
+source("SEIR_function.R")
 
 # run SIR fitting for 1-month periods
-starting_param_val = log(c(1e-2,1e-5))
+starting_param_val = c(-2.5, -3)
 date_initial = as.Date("2020-11-22")
 date_final = as.Date("2021-03-01")
 
-t1 <- sir_all(mexico, "2020-11-22", "2020-12-22", starting_param_val)
-t2 <- sir_all(mexico, "2020-12-23", "2021-01-11", t1[[3]]) 
-t3 <- sir_all(mexico, "2021-01-12", "2021-01-22", t2[[3]])
-t4 <- sir_all(mexico, "2021-01-23", "2021-03-01", t3[[3]])
+# choose method
+method = "SEIR"
 
-pred_I <- rbind(t1[[1]], t2[[1]], t3[[1]], t4[[1]]) ## bind by row
-pred_R <- rbind(t1[[2]], t2[[2]], t3[[2]], t4[[2]])
+if(method == "SIR"){
+  t1 <- sir_all(mexico, "2020-11-22", "2020-12-14", starting_param_val)
+  t2 <- sir_all(mexico, "2020-12-15", "2021-01-05", t1[[3]]) 
+  t3 <- sir_all(mexico, "2021-01-06", "2021-01-22", t2[[3]])
+  t4 <- sir_all(mexico, "2021-01-23", "2021-03-01", t3[[3]])
+  
+  pred_I <- rbind(t1[[1]], t2[[1]], t3[[1]], t4[[1]]) ## bind by row
+  pred_R <- rbind(t1[[2]], t2[[2]], t3[[2]], t4[[2]])
+}
+
+if(method == "SEIR"){
+  t1 <- seir_all(mexico, "2020-11-22", "2020-11-30", starting_param_val)
+  t2 <- seir_all(mexico, "2020-12-01", "2021-01-05", t1[[4]]) 
+  t3 <- seir_all(mexico, "2021-01-06", "2021-01-22", t2[[4]])
+  t4 <- seir_all(mexico, "2021-01-23", "2021-03-01", t3[[4]])
+  
+  pred_I <- rbind(t1[[1]], t2[[1]], t3[[1]], t4[[1]]) ## bind by row
+  pred_R <- rbind(t1[[2]], t2[[2]], t3[[2]], t4[[2]])
+}
 
 
