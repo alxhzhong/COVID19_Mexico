9d91562e4f86d72f89158eef5e70f3920ed7ed1e
alxhzhong <108833315+alxhzhong@users.noreply.github.com>
2022-07-17 18:12:51 -0400

    new dates!

--- mexicocity_dataread.R
+++ mexicocity_dataread.R
@@ -6,13 +6,13 @@
 librarian::shelf(readr, tidyr, dplyr, zoo)
 
 # Data source ---
-mxcase_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_20220713.csv"
+mxcase_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_20220716.csv"
 mx_confirmed = read_csv(mxcase_url)
 
-mxneg_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Negativos_20220713.csv"
+mxneg_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Negativos_20220716.csv"
 mx_neg = read_csv(mxneg_url)
 
-mxdeaths_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Defunciones_20220713.csv"
+mxdeaths_url = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Defunciones_20220716.csv"
 mx_deaths = read_csv(mxdeaths_url)
 
 
