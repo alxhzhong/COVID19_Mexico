178524d589b9691cfc12ddd51508d023a02768db
alxhzhong <108833315+alxhzhong@users.noreply.github.com>
2022-07-18 14:23:04 -0400

    fixing CI code to force mean to be within upper and lower... thanks Ritoban!

--- predict_CI.R
+++ predict_CI.R
@@ -44,7 +44,7 @@ ciband <- function(pred, sigma_l, sigma_u, sigma_m, beta, gamma, data, rep){
   
   pred_I_med=pred$I
   pred_R_med=pred$R
-  sd=abs(1/sigma_l+1/sigma_u-2*(1/sigma_m))/(2*1.96)
+  sd=abs(1/sigma_u + 1/sigma_l-2*(1/sigma_m))/(2*1.96)
   pred_I=matrix(0,nrow=nrow(pred),ncol=rep)
   pred_R=matrix(0,nrow=nrow(pred),ncol=rep)
 
@@ -61,9 +61,15 @@ ciband <- function(pred, sigma_l, sigma_u, sigma_m, beta, gamma, data, rep){
   }
   lwrI = round(apply(pred_I,1,quantile, probs=0.025))
   uprI = round(apply(pred_I,1,quantile, probs=0.975))
+  sd=(uprI-lwrI)/1.96*2
+  lwrI=pred_I_med-1.96*sd
+  uprI=pred_I_med+1.96*sd
   pred_I=data.frame(date,pred_I_med,lwrI,uprI)
   lwrR = round(apply(pred_R,1,quantile, probs=0.025))
   uprR = round(apply(pred_R,1,quantile, probs=0.975))
+  sd=(uprR-lwrR)/1.96*2
+  lwrR=pred_R_med-1.96*sd
+  uprR=pred_R_med+1.96*sd
   pred_R=data.frame(date,pred_R_med,lwrR,uprR)
   return(list(pred_I,pred_R))
 }
