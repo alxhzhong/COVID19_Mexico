e1fe8f9144e80ff3dfb1c2057ac2deb77a6175a1
lauren-mh16 <laurenhe@gmail.com>
2022-07-21 14:24:32 -0400

    inserted mexico flag

--- shinytesting2.R
+++ shinytesting2.R
@@ -97,7 +97,7 @@ ui <- fluidPage(tags$head(tags$style(css)), theme = shinytheme("darkly"),
                              
                              tabPanel("Home", titlePanel("Tracking and Predicting COVID-19 in Mexico"),
                                       br(),
-                                      imageOutput("mexicoFlag.png"),
+                                      imageOutput("mexicoFlag"),
                                       p("Welcome to our page for tracking and predicting COVID-19 pandemic in Mexico. Our aim is to provide a resource for individuals to visualize the COVID-19 trends in Mexico in order to better understand the situation. 
                                         some text, image ??, info about time period, policy, how to use graphs ?"),
                                       br(),
@@ -188,22 +188,14 @@ ui <- fluidPage(tags$head(tags$style(css)), theme = shinytheme("darkly"),
 server <- function(input, output, session){
   
   output$mexicoFlag <- renderImage({
-    # A temp file to save the output.
-    # This file will be removed later by renderImage
-    outfile <- tempfile(fileext = '.png')
     
-    # Generate the PNG
-    png(outfile, width = 400, height = 300)
-    hist(rnorm(input$obs), main = "Generated in renderImage()")
-    dev.off()
-    
-    # Return a list containing the filename
-    list(src = "mexicoFlag.png",
-         contentType = 'image/png',
+    filename <- "mexicoFlag.png"
+    list(src = filename, contentType = 'image/png', alt = paste("Mexico Flag"),
          width = 400,
-         height = 300,
-         alt = "This is alternate text")
-  }, deleteFile = TRUE)
+         height = 400
+  
+    )
+  })
   
   
   
