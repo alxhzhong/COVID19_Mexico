af4f2594ae44e896e38a6c248dac269a6ba6a1c8
lauren-mh16 <laurenhe@gmail.com>
2022-07-23 10:55:22 -0400

    added black line to separate model and prediction

--- prediction_graphs.R
+++ prediction_graphs.R
@@ -6,13 +6,18 @@ pred_I_SIR <- pred_I_SIR %>%
   mutate(index = 1:n()) %>% 
   mutate(loess_m = fitted(loess(pred_I_med ~ index, data = pred_I_SIR, span = 0.3))) %>% 
   mutate(upper_m = fitted(loess(uprI ~ index, data = pred_I_SIR, span = 0.3))) %>% 
-  mutate(lower_m = fitted(loess(lwrI ~ index, data = pred_I_SIR, span = 0.3)))
+  mutate(lower_m = fitted(loess(lwrI ~ index, data = pred_I_SIR, span = 0.3))) 
 
 SIR_int <- SIR_int%>% 
   left_join(pred_I_SIR, by = "date") %>% 
   left_join(pred_I, by = "date") %>% 
   left_join(pred_R_SIR, by = "date") %>% 
   left_join(pred_R, by = "date")
+SIR_int <-SIR_int %>%   mutate(
+    text = paste0("Date: ", format(date, format = '%b %d'), "<br>Actual: ",
+                  format(round(I, 2)), "<br>Model: ",
+                  paste0(format(round(SIR_int$loess_m, 2))))
+  )
 
 # SIR_I <- merge(pred_I, pred_I_SIR, by = "date", all = TRUE)
 # SIR_I <-  merge(mexico_SIR_int, SIR_I, by = "date", all = TRUE)
@@ -40,8 +45,8 @@ SEIR_int <- SEIR_int%>%
 # SEIR_I <- merge(mexico_SEIR_int, SEIR_I, by = "date", all = TRUE)
 # SEIR_R <- merge(SEIR_pred_R, pred_R_SEIR, by = "date", all = TRUE) 
 # SEIR_R <- merge(mexico_SEIR_int, SEIR_R, by = "date", all = TRUE)
-
   
+
 # SIR I prediction
 plot_ly(SIR_int, x = ~date, y = ~I, type = "bar", name = "Actual",
         color = I("#60A5E8")) %>% 
@@ -50,7 +55,10 @@ plot_ly(SIR_int, x = ~date, y = ~I, type = "bar", name = "Actual",
   add_trace(y = ~SIR_int$lwrI.y, type = 'scatter', mode = 'lines', fill = 'tonexty', fillcolor = list(color = 'rgba(100, 225, 0, 0.2)'), name = "Lower", line = list(color = "rgba(100, 225, 0, 0.2)"), hoverinfo = 'skip', showlegend = FALSE) %>% 
   add_trace(y = ~SIR_int$loess_m, type = 'scatter', mode = 'lines', line = list(color = "rgba(245, 121, 58, 1)", width = 3), name = "Model") %>%
   add_trace(y = ~SIR_int$upper_m, type = 'scatter', mode = 'lines', name = "Upper", line = list(color = "rgba(245, 121, 58, 0.2)"), showlegend = FALSE, hoverinfo = 'skip') %>% 
-  add_trace(y = ~SIR_int$lower_m, type = 'scatter', mode = 'lines', fill = 'tonexty', fillcolor = list(color = 'rgba(245, 121, 58, 0.2)'), name = "Lower", line = list(color = "rgba(245, 121, 58, 0.2)"), hoverinfo = 'skip', showlegend = FALSE) %>% 
+  add_trace(y = ~SIR_int$lower_m, type = 'scatter', mode = 'lines', fill = 'tonexty', fillcolor = list(color = 'rgba(245, 121, 58, 0.2)'), name = "Lower", line = list(color = "rgba(245, 121, 58, 0.2)"), hoverinfo = 'skip', showlegend = FALSE) %>%
+  add_lines(
+    y = range(0:max(SIR_int$pred_I_med.y, na.rm = TRUE)), x = "2021-01-12",
+    line = list(color = "black"), hoverinfo = "skip", showlegend = FALSE) %>% 
   layout(
     xaxis = list(
       range=c("2020-11-24", "2021-01-26")),
@@ -74,6 +82,9 @@ plot_ly(SEIR_int, x = ~date, y = ~I, type = "bar", name = "Actual",
   add_trace(y = ~SEIR_int$loess, type = 'scatter', mode = 'lines', line = list(color = "rgba(245, 121, 58, 1)", width = 3), name = "Model") %>%
   add_trace(y = ~SEIR_int$upper, type = 'scatter', mode = 'lines', name = "Upper", line = list(color = "rgba(245, 121, 58, 0.2)"), showlegend = FALSE, hoverinfo = 'skip') %>% 
   add_trace(y = ~SEIR_int$lower, type = 'scatter', mode = 'lines', fill = 'tonexty', fillcolor = list(color = 'rgba(245, 121, 58, 0.2)'), name = "Lower", line = list(color = "rgba(245, 121, 58, 0.2)"), hoverinfo = 'skip', showlegend = FALSE) %>% 
+  add_lines(
+    y = range(0:max(SEIR_int$pred_I_med.y, na.rm = TRUE)), x = "2021-01-12",
+    line = list(color = "black"), hoverinfo = "skip", showlegend = FALSE) %>% 
   layout(
     xaxis = list(
       range=c("2020-11-21", "2021-02-10")),
@@ -91,13 +102,13 @@ plot_ly(SEIR_int, x = ~date, y = ~I, type = "bar", name = "Actual",
 plot_ly(SIR_int, x = ~date, y = ~R, type = "bar", name = "Actual",
         color = I("#A95AA1")) %>% 
   add_trace(y = ~SIR_int$pred_R_med.x, type = 'scatter', mode = 'lines', line = list(color = 'rgb(245, 121, 58,, 1)', width = 3), name = "Model") %>%
-  # add_trace(y = ~pred_R$uprR, type = 'scatter', mode = 'lines', name = "Upper", showlegend = FALSE) %>% 
-  # add_trace(y = ~pred_R$lwrR, type = 'scatter', mode = 'lines', fill = 'tonexty', name = "Lower", showlegend = FALSE)
-  add_ribbons(ymin = ~SIR_int$lwrR.x,
-              ymax = ~SIR_int$uprR.x,
-              line = list(color = 'rgb(245, 121, 58, 0.2)'),
-              fillcolor = 'rgba(245, 121, 58, 0.2)',
-              showlegend = FALSE) %>% 
+  add_trace(y = ~SIR_int$uprR.x, type = 'scatter', mode = 'lines', name = "Upper", color = I("rgba(245, 121, 58, 0.5)"), showlegend = FALSE) %>% 
+  add_trace(y = ~SIR_int$lwrR.x, type = 'scatter', mode = 'lines', fill = 'tonexty', color = I("rgba(245, 121, 58, 0.5)"), name = "Lower", showlegend = FALSE) %>% 
+  # add_ribbons(ymin = ~SIR_int$lwrR.x,
+  #             ymax = ~SIR_int$uprR.x,
+  #             line = list(color = 'rgb(245, 121, 58, 0.2)'),
+  #             fillcolor = 'rgba(245, 121, 58, 0.2)',
+  #             showlegend = FALSE) %>% 
   add_trace(y = ~SIR_int$pred_R_med.y, type = 'scatter', mode = 'lines', line = list(color = "rgba(100, 225, 0, 1)", width = 3), name = "Prediction") %>%
   add_ribbons(ymin = ~SIR_int$lwrR.y,
               ymax = ~SIR_int$uprR.y,
@@ -105,6 +116,9 @@ plot_ly(SIR_int, x = ~date, y = ~R, type = "bar", name = "Actual",
               fillcolor = 'rgba(100, 225, 0, 0.2)',
               showlegend = FALSE,
               hoverinfo = "skip") %>% 
+  add_lines(
+    y = range(0:max(SIR_int$pred_R_med.y, na.rm = TRUE)), x = "2021-01-12",
+    line = list(color = "black"), hoverinfo = "skip", showlegend = FALSE) %>% 
   layout(
     xaxis = list(
       range=c("2020-11-24", "2021-01-26")),
@@ -130,6 +144,9 @@ plot_ly(SEIR_int, x = ~date, y = ~R, type = "bar", name = "Actual", color = I("#
   #             color = I('rgba(245, 121, 58, 0.5)'),
   #             showlegend = FALSE, hoverinfo = "none") %>% 
   add_trace(y = ~SEIR_int$pred_R_med.y, type = 'scatter', mode = 'lines', line = list(color = "rgba(100, 225, 0, 1)", width = 3), name = "Prediction") %>%
+  add_lines(
+    y = range(0:max(SEIR_int$pred_R_med.y, na.rm = TRUE)), x = "2021-01-12",
+    line = list(color = "black"), hoverinfo = "skip", showlegend = FALSE) %>% 
   layout(
     xaxis = list(
       range=c("2020-11-24", "2021-02-10")),
